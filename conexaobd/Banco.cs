adfeee464bd7aee864d6501f0d7995297b27e9d0
3etimds <3etimds@LAB2-16>
2022-03-28 10:14:37 -0300

    Adicionar arquivos de projeto.

---
+++ conexaobd/Banco.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace conexaobd
+{
+    class Banco : IDisposable
+    {
+        private readonly MySqlConnection conexao;
+
+
+        public Banco ()
+
+        {
+            conexao = new MySqlConnection (ConfigurationManager);
+            conexao.Open();
+
+        }
+
+        public void ExecutaComando(string StrQuery)
+        {
+            var vComando = new MySqlCommand
+            {
+                CommandText = StrQuery,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conexao
+
+            } 
+                
+        }
+
+        public MySqlDataReader RetornaCommando (string StrQuery)
+        {
+
+            var comando = new MySqlCommand(StrQuery, conexao);
+            return comando.ExecuteReader(); 
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException(); 
+            if (conexao.State == System.Data.ConnectionState.Open)
+                conexao.Close();
+
+        }
+    }
+}
---
+++ conexaobd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace conexaobd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = new MySqlConnection(@"Server= localhost; Database= dbExemplo; User ID= root; Password = 12345678 ");
+            connection.Open();
+            Console.WriteLine("Conectado!");
+            Console.ReadLine();
+
+            //string AtualizaUsu = "Update tbUsuario set NomeUsu = 'MeAchoEsperta' where (IDusu= 2)";
+            //MySqlCommand comandoAtualiza = new MySqlCommand(AtualizaUsu, connection);
+            //comandoAtualiza.ExecuteNonQuery();
+
+            string strinsereUsu = "INSERT INTO tbUsuario(NomeUsu, Cargo, DataNasc) VALUES('Emma', 'cerimonialista', '2020/04/07')";
+            MySqlCommand comandoApagar = new MySqlCommand(strinsereUsu, connection);
+            comandoApagar.ExecuteNonQuery();
+
+            string selectUsu = "Select * from tbUsuario";
+            MySqlCommand comando = new MySqlCommand(selectUsu, connection);
+            MySqlDataReader leitor = comando.ExecuteReader();
+            
+            while (leitor.Read()) 
+            {
+                Console.WriteLine("Id: {0}, Nome: {1}, Cargo: {2}, Data: {3}", leitor["IDusu"], leitor["NomeUsu"], leitor["Cargo"], leitor["DataNasc"]);
+            };
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
